0e9053b056a83d7c8641dcee135172de6189eacf
azizikhaidi1987 <azizi.khaidi1987@gmail.com>
2026-05-19 06:45:09 +0000

    aa

---
+++ basic.php
@@ -0,0 +1,24 @@
+<?php
+//Variables always start with $
+$name = Älice";
+$age = 25;
+$isAdmin = true;
+
+//Output to page
+echo "Hello".$name;
+
+//Conditionals
+if ($age>=18){
+    echo Adult";
+}
+else {
+        echo "Minor";
+}
+
+//Arrays
+$skills = ["HTML","CSS"];
+foreach ($skills as &s){
+    echo "<li>$s</li>";
+}
+
+?>
